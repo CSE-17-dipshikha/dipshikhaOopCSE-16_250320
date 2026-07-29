236fdb3a682cedbd5ce3de86cd3a7c06936f038a
CSE-17-dipshikha <dipshikhaarya157@gmail.com>
2026-07-29 11:11:34 +0530

    Add files via upload

---
+++ StudentCard.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ class student{
+    private:
+    int pin;
+    float balance;
+
+public:
+     
+     string reportCard;
+     string  Name;
+     string  rollNo;
+     string  marks;
+       
+ };
+
+int main() {
+   student s;
+   s.Name="Dipshikha Arya";
+   s.rollNo="75";
+   s.marks="95";
+}
